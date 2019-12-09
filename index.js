53324a606caccf0d3b08a8e15330a741e0114ad7
Jim Rountree <jimvrountree@gmail.com>
2019-12-09 22:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidaysTo(you){
   return(`Happy holidays, ${you}!`)
 
 }
-function happyHolidayTo(Independence Day, you){
-  return("Happy ${Independence Day}, ${you}!")
+function happyHolidayTo(Halloween, Danforth){
+  return("Happy ${Halloween}, ${Danforth}!")
 }
